e3354601f3c3d262c0f3778333c534874bae464e
RYAN BOONE <rboone3@ap21.uc.osg-htc.org>
2024-07-02 11:14:18 -0500

    renamed files for checksum test (sha256 -> checksum)

--- executables/checksum_ex.py
+++ executables/checksum_ex.py
@@ -7,14 +7,14 @@ Usage: use sha256sum to test for file transfer corruption
 import hashlib
 import sys
 
-with open(sys.argv[1], "rb") as output_file:
-    file_data = output_file.read()
+with open(sys.argv[1], "rb") as file:
+    file_data = file.read()
     file_hash = hashlib.sha256(file_data).hexdigest()
 
-with open(sys.argv[2], "r") as sha_file:
-    sha_file_data = sha_file.readline()[0:-1]
+with open(sys.argv[2], "r") as checksum_file:
+    checksum_data = checksum_file.readline()[0:-1]
 
-if file_hash == sha_file_data:
+if file_hash == checksum_data:
     print("file transferred successfully")
 else:
     print("file transfer error")
--- scripts/checksum_scripts.py
+++ scripts/checksum_scripts.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # Author: Ryan Boone
 """
-Usage: scripts for running sha256sum (file transfer corruption) test
+Usage: scripts for running checksum (file transfer corruption) test
 """
 
 import htcondor
@@ -10,22 +10,22 @@ import pathlib
 
 def generate_submit_files(resources: dict):
     """
-    Usage: creates submit files for the sha256sum job in Pool_Exerciser/submit_files/sha256_test/
+    Usage: creates submit files for the checksum job in Pool_Exerciser/submit_files/checksum_test/
     @param resources: dict whose keys are the GLIDEIN_ResourceName s to create submit files for
     """
     for resource in resources:
-        with open(f"./submit_files/sha256_test/{resource}.sub", "w") as file:
-            file.write("executable = ./executables/sha256_ex.py\n")
-            file.write("arguments = torus.out0.07578.h5 torus.sha256\n")
+        with open(f"./submit_files/checksum_test/{resource}.sub", "w") as file:
+            file.write("executable = ./executables/checksum_ex.py\n")
+            file.write("arguments = torus.out0.07578.h5 torus.checksum\n")
 
-            file.write("output = ./output_files/sha256_test/sha256.out\n")
+            file.write("output = ./output_files/checksum_test/checksum.out\n")
 
             file.write("request_cpus = 1\n")
             file.write("request_memory = 1GB\n")
             file.write("request_disk = 5GB\n")
 
             file.write("should_transfer_files = yes\n")
-            file.write("transfer_input_files = ./input_files/sha256_test/torus.out0.07578.h5, ./input_files/sha256_test/torus.sha256\n")
+            file.write("transfer_input_files = ./input_files/checksum_test/torus.out0.07578.h5, ./input_files/checksum_test/torus.checksum\n")
 
 #           file.write("periodic_remove = JobStatus == 1 && CurrentTime-EnteredCurrentStatus > 60*5\n")
             file.write(f"Requirements = TARGET.GLIDEIN_ResourceName == \"{resource}\"\n")
@@ -33,11 +33,11 @@ def generate_submit_files(resources: dict):
 
 def run():
     """
-    Usage: submits the sha256 test job at all sites specified by the submit files in
-           Pool_Exerciser/submit_files/sha256_test/
+    Usage: submits the test job to all sites specified by the submit files in
+           Pool_Exerciser/submit_files/checksum_test/
     """
     schedd = htcondor.Schedd()
-    submit_dir = pathlib.Path("./submit_files/sha256_test/")
+    submit_dir = pathlib.Path("./submit_files/checksum_test/")
     for file in submit_dir.iterdir():
         with open(file, "r") as submit_file:
             schedd.submit(htcondor.Submit(submit_file.read()))

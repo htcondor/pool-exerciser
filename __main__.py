b9d556eb40f5a95464a9f27980c3390deac29122
RYAN BOONE <rboone3@ap21.uc.osg-htc.org>
2024-07-29 14:49:35 -0500

    HTCSSREQ-50: updated cla parsing method with new options

--- __main__.py
+++ __main__.py
@@ -13,6 +13,7 @@ import os
 sys.path.append(os.path.join(os.path.dirname(__file__), "src"))
 from src import general
 
+
 def parse_cla() -> argparse.Namespace:
     """
     Usage: command line argument parser
@@ -20,27 +21,60 @@ def parse_cla() -> argparse.Namespace:
     """
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("test_name",
-                        type=str,
-                        metavar="test_name",
-                        choices=["sleep_test", "checksum_test"],
-                        help="name of test to be run by exerciser")
+    parser.add_argument(
+        "run-tests",
+        nargs="*",
+        default="all",
+        help="specify the test/s to run in a space seperated list. if no tests "
+        + "are listed, the exerciser will run all available tests.",
+    )
 
-    parser.add_argument("-f",
-                        "--flush-memory",
-                        action="store_true",
-                        help="clears all .sub files for {test_name}")
+    parser.add_argument(
+        "-f",
+        "--flush-all",
+        action="store_true",
+        help="deletes all subdirectories in /working",
+    )
 
-    parser.add_argument("-q",
-                        "--query-collector",
-                        action="store_true",
-                        help="queries collector for a list of current ResourceName s in the OSPool. " +
-                             "overwrites any existing .sub files which match the current list, and ignores any which do not")
+    # add ability to specify any amount of date string ex. 2024-04
+    parser.add_argument(
+        "-d",
+        "--flush-by-date",
+        metavar="YYYY-MM-DD_hh-mm",
+        help=f"deletes all subdirectories in /working older than the arg value.\n"
+        + "argument value must be given in YYYY-MM-DD_hh-mm format. "
+        + "ex: 2024-07-31_15-57",
+    )
 
-    parser.add_argument("-r",
-                        "--run",
-                        action="store_true",
-                        help="runs the exerciser on {test_name} for any existing submit files. if no submit files exist, does nothing")
+    parser.add_argument(
+        "-p",
+        "--print-tests",
+        action="store_true",
+        help="print a list of all the available exerciser tests. "
+        + "including this option prevents the exerciser from running",
+    )
+
+    parser.add_argument(
+        "-s",
+        "--snapshot",
+        action="store_true",
+        help="query the collector and print a list of currently available resources. "
+        + "including this option prevents the exerciser from running",
+    )
+
+    parser.add_argument(
+        "-w",
+        "--change-working-directory",
+        metavar="new_working_dir",
+        help="changes the location of the root working dir to the specified dir for this run"
+    )
+
+    parser.add_argument(
+        "-t",
+        "--change-test-directory",
+        metavar="new_test_dir",
+        help="changes the location of the root test dir to the specified dir for this run",
+    )
 
     return parser.parse_args()
 
@@ -49,6 +83,8 @@ def main():
     """
     Usage: run the thing
     """
+    args = parse_cla()
+
     tests_dir = Path("tests")
     working_dir = Path("working")
 
